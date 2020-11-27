02ab6ceb65aac334e67dc7dacfa24df79f52e197
冯宇航 3118005409 <820831320@qq.com>
2020-11-27 15:19:57 +0800

    排行榜

--- Easy/fecher/tool.py
+++ Easy/fecher/tool.py
@@ -3,7 +3,7 @@ from importlib import import_module
 
 async def get_novels_info(class_name, novels_name):
     novels_module = import_module(
-        "owllook.fetcher.{}.{}_novels".format('novels_factory', class_name))
+        "Easy.fetcher.{}.{}_novels".format('novels_factory', class_name))
     # 获取对应渠道实例化对象
     novels_info = await novels_module.start(novels_name)
     return novels_info
